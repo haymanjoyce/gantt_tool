e1d4fb6d9f081e7dc6eab0424d78384c84f8f272
haymanjoyce <haymanjoyce@gmail.com>
2023-03-06 18:19:40 +0000

    Added required packages

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
-import argparse
-
-import pandas
+import typer
 import svg
 import pandas as pd
 
 
 def packages():
-    print("argparse from Standard Library (alt: typer)")
-    print("svg from PyPI for programmatically creating SVG mark up (alt: drawsvg2)")
-    print("pandas from PyPI for reading CSV and Excel files (alt: openpyxl")
+    print('\nPackages:\n')
+    print("'svg' from PyPI for programmatically creating SVG mark up (alt: 'drawsvg2')")
+    print("'pandas' from PyPI for reading CSV files and more (alt: 'openpyxl')")
+    print("'typer' from PyPI for developing the CLI (alt: 'argparse')")
+    print("'colorama' from PyPI for coloured terminal text (possible 'typer' dependency)")
+    print("'shellingham' from PyPI for detecting the shell (possible 'typer' dependency)")
+    print("'pytest' from PyPI is a unit test framework")
+    print('"pyinstaller" from PyPI for turning the application into an executable (not imported)')
 
 
 def read_file(file):
